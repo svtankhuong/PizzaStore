8f7d2d48ded668964d52fe48a9b883dd7dee3dd6
HoangVu <somna124@gmail.com>
2025-03-19 00:33:12 +0700

    Nhan Vien Panel

---
+++ src/main/java/config/JDBC.java
@@ -0,0 +1,33 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JDBC {
+
+    public static Connection getConnection() {
+        Connection c = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/pizzastore";
+            String username = "root";
+            String password = "";
+            c = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            Logger.getLogger(JDBC.class.getName()).log(Level.SEVERE, "SQL connection error", e);
+        }
+        return c;
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                Logger.getLogger(JDBC.class.getName()).log(Level.SEVERE, "Error closing the connection", e);
+            }
+        }
+    }
+}
